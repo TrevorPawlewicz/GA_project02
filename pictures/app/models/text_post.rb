b8ab672db86e9913f34734a8b4af217003451570
Trev <trevor.pawlewicz@gmail.com>
2015-07-30 10:24:24 -0700

    working edit button

--- pictures/app/models/text_post.rb
+++ pictures/app/models/text_post.rb
@@ -1,6 +1,7 @@
 class TextPost < Post
     # this model inherits from the Post model
     belongs_to :user
+
     # don't allow a text post to be saved without a body:
     validates :body, presence: true
 end
